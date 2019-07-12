1402c9604a7226bfdac7ca15d7624703fe584280
DHARMESH PODDAR <dharmeshpoddar3@gmail.com>
2019-07-12 07:13:14 +0530

    Made the message list component

---
+++ src/Components/MessageList.js
@@ -0,0 +1,19 @@
+import React ,{Component} from 'react';
+
+class MessageList extends React.Component{
+   render()
+      {
+      return(
+          <div className="messagelist">
+             <div className="helpText">MessageList</div>
+
+             
+          
+          </div>
+      );
+
+
+      }
+
+}
+export default MessageList;
