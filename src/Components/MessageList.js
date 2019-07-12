98acddab8624862f8a5818928e6694844a3ee0d7
DHARMESH PODDAR <dharmeshpoddar3@gmail.com>
2019-07-12 08:54:56 +0530

    Message list component adding dummy data

--- src/Components/MessageList.js
+++ src/Components/MessageList.js
@@ -1,11 +1,40 @@
 import React ,{Component} from 'react';
 
+const Dummy_data =[
+    
+     {
+       senderID :'Dharmesh',
+       text: 'studying in lnmiit at jaipur'
+
+
+     },
+
+     {
+         senderID: 'Preet Jain',
+         text :'studies mechanical'
+
+     },
+
+     {
+         senderID:'Rohit sharma',
+         text: 'studies electrical'
+
+     }
+
+
+
+
+];
+
+
 class MessageList extends React.Component{
    render()
       {
       return(
           <div className="messagelist">
-             <div className="helpText">MessageList</div>
+            {
+                
+            }
 
              
           
